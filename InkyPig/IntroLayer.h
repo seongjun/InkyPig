f111913724a97ff692d79a0b88771484f3c6178f
Choi Seonghun <choiseonghun@Chois-MacBook-Pro.local>
2012-10-12 16:43:16 +0900

    Initial Commit

---
+++ InkyPig/IntroLayer.h
@@ -0,0 +1,21 @@
+//
+//  IntroLayer.h
+//  InkyPig
+//
+//  Created by Choi Seonghun on 12. 10. 12..
+//  Copyright __MyCompanyName__ 2012년. All rights reserved.
+//
+
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface IntroLayer : CCLayer
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
---
+++ InkyPig/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  InkyPig
+//
+//  Created by Choi Seonghun on 12. 10. 12..
+//  Copyright __MyCompanyName__ 2012년. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
